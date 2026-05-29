6456d6044cf7a23dd74f9a831ff889392926492e
nilssonna89-commits <nilssonna89@gmail.com>
2026-05-29 11:07:33 +0200

    felkod
    fixade en felkod jag missat

--- Tidsloggning/wwwroot/js/site.js
+++ Tidsloggning/wwwroot/js/site.js
@@ -5,9 +5,10 @@
 
 
 document.addEventListener("mousemove", (e) => {
+    const hero = document.querySelector(".hero");
+    if (!hero) return;
     const x = (e.clientX / window.innerWidth - 0.5) * 10;
     const y = (e.clientY / window.innerHeight - 0.5) * 10;
 
-    document.querySelector(".hero").style.transform =
-        `translate(${x}px, ${y}px)`;
+    hero.style.transform = `translate(${x}px, ${y}px)`;
 });
